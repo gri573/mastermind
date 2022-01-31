e7a81c0bc5ad9a8c704a786ade1ec1c90ed74e8b
gri573 <david.dg.grimm@gmail.com>
2022-01-31 10:03:20 +0100

    initial commit

---
+++ mastermind.py
@@ -0,0 +1,103 @@
+import numpy as np
+print("GUESS THE WORD\n--------------\n\nhow to play:\nAs the title suggests, the goal is to guess a word.\nYou will be given the amount of letters the word contains before your first guess.\nEach letter in your guess will be marked as follows:\nX - The letter is correct and in the correct location.\n* - The letter is correct, but in an incorrect location.\n_ - The letter is incorrect or you have entered it more times than it occurs in the word.\n\nType \"??\" for an in-game help menu\n")
+
+wlist = np.loadtxt("wordlist-english.txt", unpack=True, dtype=str)
+word0 = wlist[np.random.randint(0, len(wlist))]
+word0 = word0.upper()
+print("The current word contains " + str(len(word0)) + " letters.\n")
+word = []
+printstring = ""
+for letter in word0:
+    word.append(letter)
+    printstring += "_"
+printstring0 = printstring
+print(printstring)
+tries = 0
+entered = ""
+wrongletters = ""
+rightletters = ""
+while entered != word0:
+    entered = input("")
+    entered = entered.upper()
+    if entered == "??":
+        print("In-Game Help Menu\n\nOptions:\nH - get hint\nS - solve\nX - exit\n")
+        prompt = input("")
+        if prompt.upper() == "H":
+            letterremains = []
+            for letter in word:
+                found = 0
+                for letter0 in rightletters:
+                    if letter == letter0:
+                        found = 1
+                if found == 0:
+                    letterremains.append(letter)
+            if letterremains != []:
+                l = np.random.randint(0, len(letterremains))
+                printstring = ""
+                used = 0
+                for letter in word:
+                    if letterremains[l] == letter and used == 0:
+                        printstring += letter
+                        used = 1
+                    else:
+                        printstring += "-"
+                print(printstring)
+            else:
+                print("You're almost there, try harder!")
+        if prompt.upper() == "S":
+            print("solution: " + word0)
+            tries = -1
+            break
+        print(printstring0)
+        entered = input("")
+        entered = entered.upper()
+    if len(entered) != len(word):
+        print("please enter exactly " + str(len(word)) + " letters")
+    else:
+        k = 0
+        printstring = ""
+        usedletters = []
+        l = 0
+        for letter in entered:
+            if word[l] == letter:
+                usedletters.append(letter)
+                found = 0
+                for letter0 in rightletters:
+                    if letter0 == letter:
+                        found = 1
+                if found == 0:
+                    rightletters += letter
+            l += 1
+        for letter in entered:
+            if k <= len(word):
+                if word[k] == letter:
+                    printstring += "X"
+                else:
+                    found = 0
+                    for letter0 in word:
+                        if letter == letter0:
+                            found += 1
+                    if found == 0:
+                        for letter0 in wrongletters:
+                            if letter0 == letter:
+                                found = 1
+                        if found == 0:
+                            wrongletters += letter
+                        found = 0
+                    for letter0 in usedletters:
+                        if letter == letter0:
+                            found -= 1
+                    if found > 0:
+                        printstring += "*"
+                        usedletters.append(letter)
+                    else:    
+                        printstring += "_"
+                        found = 0
+            else:
+                print("unknown error, expect undefined behaviour")
+            k += 1
+        printstring += " - wrongly used letters: " + wrongletters
+        print(printstring)
+        tries += 1
+if tries >= 0:
+    print(word0 + " found in " + str(tries) + " tries")
