edc1336e1b8de9ca0dcbad0bec818e0d25f78428
gri573 <david.dg.grimm@gmail.com>
2022-02-08 13:39:50 +0100

    make wordle solver not output double entries

--- wordlesolver.py
+++ wordlesolver.py
@@ -2,6 +2,7 @@ from numpy import loadtxt
 wlist = loadtxt("wordlist-english0.txt", unpack=True, delimiter=";", dtype=str)
 unusedletters = input("available letters: ")
 word = input("known letters at correct locations (* for unknown letter): ")
+foundwords = []
 for a in wlist:
     if len(a) == len(word):
         k = 0
@@ -22,4 +23,9 @@ for a in wlist:
                 found = False
             k += 1
         if found == True:
-            print(a)
+            for prev in foundwords:
+                if prev == a.upper():
+                    found = False
+            if found == True:
+                foundwords.append(a.upper())
+                print(a)
